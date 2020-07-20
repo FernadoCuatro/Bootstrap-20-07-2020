02b4901a91ffff2ae65b3bad98e986ff77bfad65
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2020-07-19 21:33:07 -0600

    Enviado sin el index, poco para terminar el curso

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// TIEMPO PARA EL SLIDER
+// $('.carousel').carousel({
+// 	interval:3000,
+// 	pause:'hover'
+// })
+
+//MOSTRAR MODAL AUTOMATICAMENTE
+// $('#miModal').modal({
+// 	show: true
+// })
+
+$('[data-toggle="popover"]').popover();
+
